d269f72bfc308a3ac79802f45b2199c53827c042
Thom Veldpaus <thompoppins@gmail.com>
2023-10-25 20:58:50 +0100

    Clean Vite install to migrate into from files in the frontend backup directory

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
-import { Provider } from "react-redux";
-import store from "./store/store.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <SnackbarProvider>
-        <App />
-      </SnackbarProvider>
-    </Provider>
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

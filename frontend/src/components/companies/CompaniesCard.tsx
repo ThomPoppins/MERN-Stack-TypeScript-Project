8c044596126874bcb893be733d16aa9ba9d5ffa4
Thom Veldpaus <thompoppins@gmail.com>
2023-11-01 11:00:59 +0100

    Quick fix type errors

--- frontend/src/components/companies/CompaniesCard.tsx
+++ frontend/src/components/companies/CompaniesCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CompaniesSingleCard from "./CompaniesSingleCard";
 
-const CompaniesCard = ({ companies, updateCompanies }) => {
+interface CompaniesCardProps {
+  companies: Array<object>;
+  updateCompanies: () => void;
+}
+
+const CompaniesCard = ({ companies, updateCompanies }: CompaniesCardProps) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {companies.map((item) => (
+      {companies.map((item: any) => (
         <CompaniesSingleCard company={item} updateCompanies={updateCompanies} />
       ))}
     </div>
